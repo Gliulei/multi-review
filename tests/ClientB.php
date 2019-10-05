7fbe11de57cbe0635f03f76d06e027e600fd9ce1
liulei <1245332635@qq.com>
2019-10-05 21:29:45 +0800

    tests

---
+++ tests/ClientB.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: liulei
+ * Date: 2019/10/5
+ * Time: 9:12 PM
+ */
+class ClientB
+{
+    public function test()
+    {
+        echo __FUNCTION__;
+    }
+}
--- tests/ClientTest.php
+++ tests/ClientTest.php
@@ -6,7 +6,26 @@
  * Date: 2019/10/3
  * Time: 8:42 PM
  */
+require_once '../vendor/autoload.php';
+
+use Review\Task;
+
 class ClientTest
 {
+    public function wait()
+    {
+//        $redis = new Redis();
+//        $redis->pconnect('127.0.0.1', '6379');
+        $redis = new stdClass();
+        $task = new Task($redis, $redis);
+        $ids = $task->setTaskKey('demo')->setCallback(function () {
+            $B = new ClientB();
+            $B->test();
+        })->getWaitTaskIds();
 
+        return $ids;
+    }
 }
+
+$clientTest = new ClientTest();
+$clientTest->wait();
